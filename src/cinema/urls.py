a1b6484bde96e339025c7d5f29fd6cbdc191ad6c
nuriya <nuriya434@mail.ru>
2024-03-22 01:10:20 +0600

    пагинация и фильтрация

--- src/cinema/urls.py
+++ src/cinema/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import MovieFilterView, movie_manage, movie_detail, create_movie, delete_movie, update_movie, session_list, session_detail, movie_list
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 urlpatterns = [
     path('', movie_manage, name='home'),
     path('movies/', movie_manage, name='movie_manage'),  
@@ -13,4 +17,9 @@ urlpatterns = [
     path('movies/', movie_list, name='movie_list'),
     path('movies/filter/', MovieFilterView.as_view(), name='movie_filter'), 
     path('movie/<int:movie_id>/', movie_detail, name='movie_detail'),
+
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
